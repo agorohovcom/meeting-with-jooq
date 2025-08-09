7584b3d31ef36218797f35d15784201d831f0921
agorohov <aleksandr.gorohov@gmail.com>
2025-08-09 17:15:07 +0300

    init commit

---
+++ src/main/java/com/agorohov/meeting_with_jooq/MeetingWithJooqApplication.java
@@ -0,0 +1,13 @@
+package com.agorohov.meeting_with_jooq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingWithJooqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingWithJooqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agorohov/meeting_with_jooq/MeetingWithJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agorohov.meeting_with_jooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingWithJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
